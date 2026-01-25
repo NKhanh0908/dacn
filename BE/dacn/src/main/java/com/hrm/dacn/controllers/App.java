11048f6a748ad7b1dd5d24c8758a7c2f1e7779e2
vu <nguyenhoa.070781@gmail.com>
2026-01-25 15:26:05 +0700

    employee

--- BE/dacn/src/main/java/com/hrm/dacn/controllers/App.java
+++
@@ -1,4 +0,0 @@
-package com.hrm.dacn.controllers;
-
-public class App {
-}
---
+++ BE/dacn/src/main/java/com/hrm/dacn/controllers/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.hrm.dacn.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hrm.dacn.dtos.APIResponse;
+import com.hrm.dacn.dtos.Employee.Request.EmployeeCreateRequest;
+import com.hrm.dacn.dtos.Employee.Response.EmployeeResponse;
+import com.hrm.dacn.entities.Employee;
+import com.hrm.dacn.services.EmployeeService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeService getEmployeeService() {
+        return employeeService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<EmployeeResponse> createEmployee(@RequestBody EmployeeCreateRequest createRequest) {
+        EmployeeResponse createdEmployee = employeeService.create(createRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdEmployee);
+
+    }
+
+}
